49b02ac6ec644e7cb5eb5afe7249f37b05b5b28d
markus knauer <knauer.kar2009@gmail.com>
2021-12-14 16:31:21 -0300

    Att

--- src/components/SideBar/styled.js
+++ src/components/SideBar/styled.js
@@ -54,7 +54,7 @@ export const AreaSidebar = styled.div`
   }
 
   @media screen and (max-width: 820px) {
-    position: absolute;
+    position: fixed;
     display: flex;
     width: 100%;
     height: 20%;
